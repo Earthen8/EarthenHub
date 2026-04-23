e36c691ab9d52dc91da3fd6a67d08f31395d9708
unknown <earthen505@gmail.com>
2026-04-23 20:36:35 +0700

    docs: Create a Design Tokens File

---
+++ frontend/src/constants/tokens.ts
@@ -0,0 +1,15 @@
+export const COLORS = {
+  primary: "#FFDB70",
+  secondary: "#1E1E1F",
+  tertiary: "#D6D6D6",
+  neutral: "#121212",
+}
+
+export const TYPOGRAPHY = {
+  fontFamily: "Manrope, sans-serif",
+  scale: {
+    headline: "2rem",
+    body: "1rem",
+    label: "0.875rem",
+  }
+}
