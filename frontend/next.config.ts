b0eb3f44d25663c1d08ff05b09254ced2cc841eb
unknown <earthen505@gmail.com>
2026-05-14 23:12:35 +0700

    fix: debug image rendering issue on projects

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ['100.107.249.94'],
+  async rewrites() {
+    return [
+      {
+        source: '/media/:path*',
+        destination: 'http://127.0.0.1:8000/media/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
