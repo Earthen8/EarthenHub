7b4c1a71f618ac38ea9ea38f14be8e59a5d8aadf
unknown <earthen505@gmail.com>
2026-05-15 12:19:09 +0700

    feat: initialize core URL configuration with API routing and media/static file serving

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -20,6 +20,9 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.urls import re_path
+from django.views.static import serve
+
 urlpatterns = [
     path('api/', include([
         path('admin/', admin.site.urls),
@@ -27,5 +30,8 @@ urlpatterns = [
     ])),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve media and static files in production
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+]
