f243b3eaf007fbcb4a26701bdc1f336bb2cad238
unknown <earthen505@gmail.com>
2026-05-20 21:25:09 +0700

    feat: configure DRF router and URL patterns for backend API endpoints

--- backend/earthenhub/urls.py
+++ backend/earthenhub/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
 
-router = DefaultRouter(trailing_slash=False)
+router = DefaultRouter()
 router.register(r'disciplines', views.DisciplineViewSet)
 router.register(r'projects', views.ProjectViewSet)
 router.register(r'experiences', views.ExperienceViewSet)
